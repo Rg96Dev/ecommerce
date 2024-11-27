320440c9d4c5fef0c7ed21d2287ea14a0ea3ba22
Rafael Guilherme <rafal.gui96@gmail.com>
2024-11-27 17:39:07 -0300

    adicionando o header e suas configurações

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 //função que seleciona os botoes
-document.addEventListener("DOMContentLoaded", function (params) {
+document.addEventListener("DOMContentLoaded", function () {
     const buttons = document.querySelectorAll("[data-tab-button]");
     const question = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector(".hero");
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener("scroll", function () {
+        const posicaoAtual = window.scrollY;
+
+
+        if (posicaoAtual < alturaHero) {
+            exibeElementosDoHeader();
+        }
+        else {
+            ocultaElementosDoHeader();
+
+        }
+
+    })
+
+
+    //efeitos seção faq
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener("click", function (botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -15,22 +34,32 @@ document.addEventListener("DOMContentLoaded", function (params) {
 
     }
 
-//efeitos seção faq
-for (let i = 0; i < question.length; i++) {
-    question[i].addEventListener("click", abreOuFechaResposta);
-    
-}
+    for (let i = 0; i < question.length; i++) {
+        question[i].addEventListener("click", abreOuFechaResposta);
+
+    }
 });
+
+// funções que controlam o menu de navegação
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
 
 // Função que abre e fecha abas de perguntas
 function abreOuFechaResposta(elemento) {
-     // Obtem o elemento pai do alvo do evento (o botão ou a pergunta clicada)
+
+    // Obtem o elemento pai do alvo do evento (o botão ou a pergunta clicada)
     const classe = "faq__questions__item--is-open";
     const elementoPai = elemento.target.parentNode;
 
-     // Adiciona ou remove a classe no elemento pai
-    elementoPai.classList.toggle(classe);
-    
+    elementoPai.classList.toggle(classe); // Adiciona ou remove a classe no elemento pai
+
 }
 
 //função definida para remover a classe ativa do botão
@@ -46,9 +75,9 @@ function removeBotaoAtivo() {
 function escondeAbas() {
     const tabsContainer = document.querySelectorAll("[data-tab-id]");
 
-    for (let i  = 0; i < tabsContainer.length; i++) {
+    for (let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove("shop__nav--is-active");
-        
+
     }
 }
 
